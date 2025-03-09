98299412a9394c65e7530f270f97243ec1885c4b
illuminatianon <me@illuminati.com>
2025-03-09 00:09:28 -0500

    Update cursorrules and disable a lint issue

--- server/index.js
+++ server/index.js
@@ -84,6 +84,7 @@ app.use('/api/config', configRouter);
 app.use('/api/generation', generationRouter);
 
 // Error handling
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   logger.error(err, 'Unhandled error');
   res.status(err.status || 500).json({
